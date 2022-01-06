61364ced4a26cb9ea2155858b67c13795d5fbec4
dahn510 <dahn510@gmail.com>
2022-01-05 16:32:00 -0800

    fixed spelling error on Config method

--- backend/database/__init__.py
+++ backend/database/__init__.py
@@ -18,12 +18,12 @@ class Config():
     try:
         # Values are loaded as dict
         config: Dict[str, Any] = dotenv_values(".env")
-        # __ in front of variable name to tells the interpreter to change variable name
-        __sql_name = str(config["SQL_DATABASE_NAME"])
-        __sql_user = str(config["SQL_USER_NAME"])
-        __sql_password = str(config["SQL_PASSWORD"])
-        __sql_host = str(config["SQL_HOST"])
-        __sql_port = str(config["SQL_PORT"])
+        # _ in front of variable name for weak proctection 
+        _sql_name = str(config["SQL_DATABASE_NAME"])
+        _sql_user = str(config["SQL_USER_NAME"])
+        _sql_password = str(config["SQL_PASSWORD"])
+        _sql_host = str(config["SQL_HOST"])
+        _sql_port = str(config["SQL_PORT"])
 
     except KeyError as err:
         print("Error from ", __name__," in", __file__, ": Could not find key ", err, file=sys.stderr)
@@ -35,35 +35,35 @@ class Config():
     """
     @classmethod
     def sql_dbname(cls) -> str:
-        return cls.__sql_name
+        return cls._sql_name
 
     """
     Returns(string) username for SQL database
     """
     @classmethod
     def sql_user(cls) -> str:
-        return cls.__sql_user
+        return cls._sql_user
 
     """
     Returns(string) password for SQL database
     """
     @classmethod
     def sql_password(cls) -> str:
-        return cls.__sql_password
+        return cls._sql_password
 
     """
     Returns(string) host for SQL database
     """
     @classmethod
     def sql_host(cls) -> str:
-        return cls.__sql_host
+        return cls._sql_host
 
     """
     Returns(string) port for SQL database
     """
     @classmethod
-    def sql_name(cls) -> str:
-        return cls.__sql_port
+    def sql_port(cls) -> str:
+        return cls._sql_port
 
 
 
